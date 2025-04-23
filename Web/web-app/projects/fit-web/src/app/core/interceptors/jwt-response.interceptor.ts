e17cb314d83c6ee94aa8fd37124fa687f5f81846
Adam <adam91mko@gmail.com>
2025-04-23 12:46:36 +0200

    Production fixes

--- Web/web-app/projects/fit-web/src/app/core/interceptors/jwt-response.interceptor.ts
+++ Web/web-app/projects/fit-web/src/app/core/interceptors/jwt-response.interceptor.ts
@@ -7,6 +7,10 @@ const AUTH_PATH: string = 'api/main/users/login';
 export function jwtResponseInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
   return next(req).pipe(
     tap((res) => {
+      if (res instanceof HttpResponse) {
+        console.log(res?.url as string)
+      }
+
       if (res instanceof HttpResponse && urlPath.extract(res.url as string) === AUTH_PATH) {
         const response = res as HttpResponse<unknown>;
         if (response.headers.has("Authorization")) {
