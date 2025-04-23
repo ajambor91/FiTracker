4486981bd92865d15d221ddff7169fb3df1d33e9
Adam <adam91mko@gmail.com>
2025-04-23 12:54:45 +0200

    Production fixes

--- Web/web-app/projects/fit-web/src/app/core/utils/url-path.util.ts
+++ Web/web-app/projects/fit-web/src/app/core/utils/url-path.util.ts
@@ -1,6 +1,10 @@
+import {environment} from '../../../environments/environment';
+
 const regexp: RegExp = /(?<=^[htps]*:\/\/[a-zA-Z0-9\-]*\.[a-zA-Z]*\/).*/g;
+const prodRegexp: RegExp = /(?<=^[htps]*:\/\/[a-zA-Z0-9\-]*\.[a-zA-Z0-9\-]*\.[a-zA-Z]*\/).*/g;
+const currentRegexp: RegExp = environment.production ? prodRegexp : regexp;
 const extract = (url: string) => {
-  const path: RegExpMatchArray | null = regexp.exec(url);
+  const path: RegExpMatchArray | null = currentRegexp.exec(url);
   if (path == null || path.length === 0) {
     return null;
   }
